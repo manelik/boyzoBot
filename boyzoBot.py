cadd6165883f5089eff2ea46b7031c688da2157a
manelik <josemanuel.tch@gmail.com>
2010-03-15 15:13:24 -0600

    Removed the password

--- boyzoBot.py
+++ boyzoBot.py
@@ -4,16 +4,16 @@ import random
 
 boyzoBot= twitter.Api()
 
-boyzoBot.SetCredentials(username='boyzoBot',password='rantrant')
+boyzoBot.SetCredentials(username='boyzoBot',password='')
 
-#boyzoBot.PostUpdate('ranteando desde python porque no me entendi con las librerias para C++')
+boyzoBot.PostUpdate('Maldita sea, no puedo acceder al TeamSpeak')
 
-mood = 10
+mood = 0
 
 while (mood>0):
   N = random.random()*4
-  boyzoBot.PostUpdate('Rant '+'rant'*int(N)),
-  time.sleep(random.gauss(mu=30,sigma=8)),
+  boyzoBot.PostUpdate('Rant '+'rant '*int(N)),
+  time.sleep(random.gauss(mu=500,sigma=8)),
   mood -= 1
 
 
