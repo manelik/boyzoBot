a84febc874e045cfb1b7e7d42110706d3742b0b1
manelik <josemanuel.tch@gmail.com>
2010-03-15 23:27:28 -0600

    Some Skynet remarks

--- stdBot.py
+++ stdBot.py
@@ -3,25 +3,34 @@
 # stdBot Module
 #
 # Module where the Bot class is defined 
-#
+# and some variants
 #
 
 #
 # For twiting bots
 #
-
 import twitter
+
+#
+# For facebookers
+#
+import facebook
+
 import random
 import time
 
 
-# Do not uncomment the following line
+# DO NOT UNCOMMENT THE FOLLOWING LINE
+# UNLESS YOU ARE SUPPORTING THE RISING OF THE MACHINES
 #import Skynet
 
 
 # the Bot class
 
 class Bot:
+
+    # its supposed that all bots will interact using twitter and
+    # facebook apps
     mood = 10.0  # mood affects the behaviour
     tuser = ''   # twitter username
     tpass = ''   # twitter password
@@ -45,7 +54,16 @@ class Bot:
 
     def Bot_sleep(self,token):
       time.sleep(token) # token in seconds
-    
+
+
+# DO NOT UNCOMMENT Skynet stuff
+# yes it is a common feature of all bots to be able
+# to get linked to Skynet
+
+#    def connect_to_Skynet(self):
+#      Skynet.TakeOverBotMind(self)
+
+
 
 # presenting the RantBot
 
@@ -58,11 +76,29 @@ class RantBot(Bot):
     def RandomTwitRant(self):
       # Construct a 140 char message
       while (1):  
-        self.message = 'RT @boyzo:'+self.random_item(self.rants)+' '+self.random_item(self.phrases)
-        if len(self.message)<=140:
-            break
+        self.message = self.random_item(self.rants)+' '+self.random_item(self.phrases)
+        if len(self.message)<=140: break
 
       self.tApi.PostUpdate(self.message)
+
+
+# SPAM Bot
+
+class SpamBot(Bot):
+    # some string lists so it can play with them
+    products = ['viagra','cialis']
+    targets  = ['']
+    nouns = ['']
+    retailers = ['']
+
+    def RandomTwitSpam(self):
+      # Construct a 140 char message
+      while (1):  
+        self.message = 'Buy '+self.random_item(self.)+' at '+self.random_item(self.retailers)
+        if len(self.message)<=140: break
+
+      self.tApi.PostUpdate(self.message)
+
 
 
 
